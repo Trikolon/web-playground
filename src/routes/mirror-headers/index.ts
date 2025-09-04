c7afae20277364d87fe8b5c9d2ef8b51345e5ca7
Emma Zuehlcke <emz@mozilla.com>
2025-09-04 19:37:22 +0200

    Add mirror-headers route.
    Returns the client's request headers as JSON

---
+++ src/routes/mirror-headers/index.ts
@@ -0,0 +1,16 @@
+// title: Returns the client's request headers as JSON
+
+const route: ExportedHandler<Env>["fetch"] = async (request, env, ctx) => {
+	// Get all request headers as an object
+	const headersObj: Record<string, string> = {};
+	for (const [key, value] of request.headers.entries()) {
+		headersObj[key] = value;
+	}
+	return new Response(JSON.stringify(headersObj, null, 2), {
+		headers: {
+			"content-type": "application/json"
+		}
+	});
+};
+
+export default route;
